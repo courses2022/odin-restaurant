857795f87db0dfb6d9856385ded65f13857fd51e
rajpranz <rajeshwarrprasad@yahoo.com>
2022-06-14 19:26:53 +1200

    setup webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+import './style.css'
+import Logo from './logo.png'
+
 console.log("Hello World");
+
+function component() {
+    const element = document.createElement('div');
+ 
+    // Lodash, now imported by this script
+    element.innerHTML ="<h2>Hello webpack</h2>";
+    element.classList.add('hello');
+ 
+   // Add the image to our existing div.
+   const myIcon = new Image();
+   myIcon.src = Logo;
+ 
+   element.appendChild(myIcon);
+ 
+    return element;
+  }
+ 
+ // document.body.appendChild(component());
+
