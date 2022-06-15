95608e056a8b4e0ea10b7fc4709b1cbdecf3d093
rajpranz <rajeshwarrprasad@yahoo.com>
2022-06-15 21:20:33 +1200

    Add menu item images

--- src/menuPage.js
+++ src/menuPage.js
@@ -1,6 +1,32 @@
+import BulDak from './BulDak.jpeg';
+
 const menuPage = () => {
     const element = document.createElement('div');
-    element.innerHTML = "Menu Page";
+    element.classList.add('menuGrid');
+    
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const img = document.createElement('img');
+    img.classList.add('menuImg');
+    img.src = Logo;
+    card.appendChild(img);
+
+    const itemName = document.createElement('p');
+    itemName.innerText = "BUL DAK";
+    card.appendChild(itemName);
+
+    const itemDesc = document.createElement('p');
+    itemDesc.innerText = "Korean style Beef BBQ sautéed with onion, scrambled egg, house made green salad on steamed rice. Two special sauces, black sauce and white sauce.";
+    card.appendChild(itemDesc);
+
+    const card2 = card.cloneNode(true);
+    const card3 = card.cloneNode(true);
+
+    element.appendChild(card);
+    element.appendChild(card2);
+    element.appendChild(card3);
+
 
     return element;
 
