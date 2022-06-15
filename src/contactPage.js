3cc77c0a3a6603c25ffbfbe143650b96c69792b6
rajpranz <rajeshwarrprasad@yahoo.com>
2022-06-15 19:51:51 +1200

    Add modules for contact and menu page

---
+++ src/contactPage.js
@@ -0,0 +1,9 @@
+
+const contactPage = () => {
+    const element = document.createElement('div');
+    element.innerHTML = "Contact Page";
+
+    return element;
+}
+
+export {contactPage};
---
+++ src/menuPage.js
@@ -0,0 +1,9 @@
+const menuPage = () => {
+    const element = document.createElement('div');
+    element.innerHTML = "Menu Page";
+
+    return element;
+
+}
+
+export {menuPage};
